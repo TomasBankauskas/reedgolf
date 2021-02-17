0ea47e1407c260b67dafb836598df14a416859a9
aspiringpro <marcusjamesreed@gmail.com>
2021-02-17 07:53:57 +0000

    .

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ import Footer from './Footer';
 export default class Body extends React.Component {
     
     render() {
-        const identity = useIdentityContext()
+        identity = useIdentityContext()
         return (
             <React.Fragment>
                 <Helmet>
