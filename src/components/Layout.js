e63c0ff4a1e3db8a3f5f3f1f94823bfe77021df8
aspiringpro <marcusjamesreed@gmail.com>
2021-02-17 08:11:07 +0000

    .

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ import Footer from './Footer';
 export default class Body extends React.Component {
     constructor(props) {
         super(props);
-        this.identity = this.useIdentityContext().bind(this)
+        identity = useIdentityContext()
     }
     
 
